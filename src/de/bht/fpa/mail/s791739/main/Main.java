b06aa0b0e1d3027b944059b05eb11e6bb647b5e5
Marco <s55090@beuth-hochschule.de>
2014-10-20 13:32:56 +0200

    erster comment

---
+++ src/de/bht/fpa/mail/s791739/main/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bht.fpa.mail.s791739.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Kobe
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/de/bht/fpa/mail/s791739/view/FXMLDocument.fxml"));
+        
+        
+        Scene scene = new Scene(root,900,600);
+       
+        
+        stage.setScene(scene);
+        stage.setTitle("FPA Mailer");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
