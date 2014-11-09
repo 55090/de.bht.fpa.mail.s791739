e0c4f3b2774ad4c94cdcb462a98a4b7c5548776e
András Bucsi <bucsi@beuth-hochschule.de>
2014-11-09 20:00:50 +0100

    upload current version

---
+++ src/de/bht/fpa/mailApp/s791739/main/Main.java
@@ -0,0 +1,32 @@
+package de.bht.fpa.mailApp.s791739.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Start class for Application
+ * @author András Bucsi
+ * @version 1.0
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+	Parent root = FXMLLoader.load(getClass().getResource("/de/bht/fpa/mailApp/s791739/view/FXMLDocument.fxml"));
+	Scene scene = new Scene(root);
+	
+	stage.setScene(scene);
+	stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	launch(args);
+    }
+
+}
