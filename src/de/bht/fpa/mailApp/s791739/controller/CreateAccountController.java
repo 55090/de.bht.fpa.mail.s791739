7a9502ec44232e4220f9d52d8f1001705bebacaa
Marco <s55090@beuth-hochschule.de>
2015-01-21 13:25:11 +0100

    test commit

---
+++ src/de/bht/fpa/mailApp/s791739/controller/CreateAccountController.java
@@ -0,0 +1,14 @@
+package de.bht.fpa.mailApp.s791739.controller;
+
+/**
+ *
+ * @author Kobe
+ */
+public class CreateAccountController {
+    private final MainViewController mvcontroller;
+    public CreateAccountController(final MainViewController mvcontroller ){
+        this.mvcontroller = mvcontroller; 
+    }
+    
+    
+}
---
+++ src/de/bht/fpa/mailApp/s791739/model/applicationLogic/account/AccountManager.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bht.fpa.mailApp.s791739.model.applicationLogic.account;
+
+import de.bht.fpa.mailApp.s791739.model.data.Account;
+import java.util.List;
+
+/**
+ *
+ * @author Kobe
+ */
+public class AccountManager implements AccountManagerIF{
+
+    @Override
+    public Account getAccount(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Account> getAllAccounts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean saveAccount(Account acc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateAccount(Account account) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
